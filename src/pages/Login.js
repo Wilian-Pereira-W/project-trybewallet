b129674136dc19893e9e744d1c8469c78a22e14c
Wilian Pereira <wilian_pereira92@outlook.com>
2021-11-19 21:25:34 -0300

    Adicionado Requisito 1 e 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,101 @@
+import PropTypes from 'prop-types';
 import React from 'react';
+import { connect } from 'react-redux';
+import { inputEmail } from '../actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      enableButton: true,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.validateInput = this.validateInput.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange(event) {
+    const { name } = event.target;
+    const { value } = event.target;
+
+    this.setState({
+      [name]: value,
+    }, this.validateInput());
+  }
+
+  handleClick(event) {
+    event.preventDefault();
+    const { history, getEmail } = this.props;
+    history.push('/carteira');
+    const { email } = this.state;
+    getEmail(email);
+  }
+
+  validateInput() {
+    const { password, email } = this.state;
+    const reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+    const passwordLength = password.length;
+    const minimalCharacter = 5;
+    if (passwordLength >= minimalCharacter && reg.test(email)) {
+      this.setState({
+        enableButton: false,
+      });
+    } else {
+      this.setState({
+        enableButton: true,
+      });
+    }
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password, enableButton } = this.state;
+    return (
+      <form>
+        <label htmlFor="email-input">
+          Email:
+          <input
+            data-testid="email-input"
+            type="email"
+            id="email-input"
+            name="email"
+            value={ email }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="password-input">
+          Password:
+          <input
+            data-testid="password-input"
+            type="password"
+            id="password-input"
+            name="password"
+            value={ password }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <button
+          type="submit"
+          disabled={ enableButton }
+          onClick={ this.handleClick }
+        >
+          Entrar
+        </button>
+      </form>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  getEmail: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  getEmail: (state) => dispatch(inputEmail(state)),
+});
+
+export default connect(null, mapDispatchToProps)(Login);
